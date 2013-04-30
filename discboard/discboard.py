a5cc6c655fb7e02d5bd688d5385f05341cfb75bb
Howard Mao <zhehao.mao@gmail.com>
2013-04-29 21:12:26 -0400

    have discboard.py create database if needed

--- discboard/discboard.py
+++ discboard/discboard.py
@@ -1,5 +1,8 @@
-from application import app
+from application import app, db
 import routes
+import os
 
 if __name__ == '__main__':
+    if not os.path.isfile('/tmp/example.db'):
+        db.create_all()
     app.run()
